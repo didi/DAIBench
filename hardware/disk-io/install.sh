f448229749c4ca9510c2d952546f48296f33b8da
赵永科 <zhaoyongke@192.168.0.101>
2022-09-09 00:00:30 +0800

    fix fio install scripts

--- hardware/disk-io/install.sh
+++ hardware/disk-io/install.sh
@@ -2,11 +2,12 @@
 source /etc/os-release
 case $ID in
 ubuntu|debian)
-  sudo apt install -y fid
+  sudo apt update
+  sudo apt install -y fio
   ;;
 centos|rhel)
   sudo yum install -y epel-release
-  sudo yum install -y fid
+  sudo yum install -y fio
   ;;
 *)
         exit 1
