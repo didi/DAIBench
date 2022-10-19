8698b6b6b50d156fb394add1a5fb9d447fa90b01
root <root@ml-p4-ser003-c09.us01>
2022-10-19 17:36:38 +0800

    4090 test support

--- L1_HardwareBenchmarks/14_gpu_all_test/install.sh
+++ L1_HardwareBenchmarks/14_gpu_all_test/install.sh
@@ -14,16 +14,7 @@ centos|rhel)
 	;;
 esac
 
-
-GPU_DRIVER_VERSION=515.65.01
-
-wget https://cn.download.nvidia.cn/tesla/$GPU_DRIVER_VERSION/NVIDIA-Linux-x86_64-$GPU_DRIVER_VERSION.run
-chmod 755 NVIDIA-Linux-x86_64-$GPU_DRIVER_VERSION.run
-./NVIDIA-Linux-x86_64-$GPU_DRIVER_VERSION.run
-rm -f NVIDIA-Linux-x86_64-$GPU_DRIVER_VERSION.run
-
-CUDA_TOOLKIT_VERSION=11.7.1
-wget https://developer.download.nvidia.com/compute/cuda/$CUDA_TOOLKIT_VERSION/local_installers/cuda_${CUDA_TOOLKIT_VERSION}_${GPU_DRIVER_VERSION}_linux.run
-chmod 755 cuda_${CUDA_TOOLKIT_VERSION}_${GPU_DRIVER_VERSION}_linux.run
-./cuda_${CUDA_TOOLKIT_VERSION}_${GPU_DRIVER_VERSION}_linux.run
-rm -f cuda_${CUDA_TOOLKIT_VERSION}_${GPU_DRIVER_VERSION}_linux.run
+wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
+chmod 755 cuda_11.8.0_520.61.05_linux.run
+./cuda_11.8.0_520.61.05_linux.run
+rm -f cuda_11.8.0_520.61.05_linux.run
