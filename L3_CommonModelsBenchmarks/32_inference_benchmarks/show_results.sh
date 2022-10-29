899595e6c5039fd90582b5e4b5729d6fc8d822e0
Mo Jianhao <mojianhao@jiansuan.tech>
2022-10-29 14:47:36 +0800

    1. Modify inference show_results.sh to generate csv file; 2. Add compare_results.py to compare csv and plot

--- L3_CommonModelsBenchmarks/32_inference_benchmarks/show_results.sh
+++ L3_CommonModelsBenchmarks/32_inference_benchmarks/show_results.sh
@@ -8,6 +8,7 @@ fi
 
 WORKDIR=$1
 
+echo -e "Model,Batch,FP32,TF32,FP32*,TF32*,FP16,INT8" > results.csv
 for model in alexnet googlenet resnet18 resnet50 resnet101 resnet152 vgg16 vgg19
 do
   echo -e "===================${model}===================="
@@ -22,6 +23,7 @@ do
     fp16=`cat $WORKDIR/${model}_batch${batch}_fp16.log | grep "Throughput" | grep "qps" | awk '{print $4}'`
     int8=`cat $WORKDIR/${model}_batch${batch}_int8.log | grep "Throughput" | grep "qps" | awk '{print $4}'`
     echo -e "$batch\t$fp32\t$tf32\t$fp32_g\t$tf32_g\t$fp16\t$int8"
+    echo -e "$model,$batch,$fp32,$tf32,$fp32_g,$tf32_g,$fp16,$int8" >> results.csv
     batch=`expr $batch \* 2`
   done
 done
